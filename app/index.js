3fc3ad04cb74511a009e02971e9a728f7e9a62a2
Emir Karaaslan <karaaslan.emir44@gmail.com>
2025-08-05 17:01:47 +0300

    Final Commit CI/CD pipeline is literally working

--- app/index.js
+++ app/index.js
@@ -4,21 +4,8 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
-    res.send('Boom Bitch  !!!');
+    res.send('Boom Bitch ');
+
 });
 
 app.listen(port, () => {
