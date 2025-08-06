39d2113e0ff4eba6874c1628739d11e7271fe460
Emir Karaaslan <karaaslan.emir44@gmail.com>
2025-08-06 13:53:24 +0300

    test

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hallo Welt !!!');
+    res.sendFile(path.join(__dirname, 'index.html'));
+
 
 });
 
